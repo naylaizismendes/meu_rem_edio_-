c22344222dd2c185d4b0923e61706cef6fb1bbad
Nayla Izis Mendes Ferreira <114931127+naylaizismendes@users.noreply.github.com>
2023-11-19 21:54:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 from kivymd.app import MDApp
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager
+from kivy.lang import Builder
 
-class App(MDApp):
+class Ui(ScreenManager):
     pass
 
-if __name__=="__main__" :
-    App().run()
+class MainApp(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = 'Dark'
+        self.theme_cls.primary_palette = 'Teal'
+        Builder.load_file('main.kv')  # Use load_file instead of load_string
+        return Ui()
+    def change_style(self,  checked, value):
+        if value:
+            self.theme_cls.theme_style = 'Dark'
+        else:
+            self.theme_cls.theme_style = 'Light'
+if __name__=="__main__":
+    MainApp().run()
+
