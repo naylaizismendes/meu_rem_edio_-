97c57e5ab99f6000f9e3e96e2aa087663ec6f123
Nayla Izis Mendes Ferreira <114931127+naylaizismendes@users.noreply.github.com>
2023-11-24 16:17:04 -0300

    Create alam.py

---
+++ alam.py
@@ -0,0 +1,50 @@
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivymd.app import MDApp
+from kivymd.uix.button import MDRaisedButton
+from kivymd.uix.textfield import MDTextField
+from kivy.clock import Clock
+
+Builder.load_string("""
+<MyLayout>:
+    orientation: "vertical"
+
+    MDTextField:
+        id: medicine_input
+        hint_text: "Medicamento"
+        size_hint_x: None
+        width: 300
+
+    MDTextField:
+        id: time_input
+        hint_text: "Horário (HH:MM)"
+        size_hint_x: None
+        width: 300
+
+    MDRaisedButton:
+        text: "Adicionar Medicamento"
+        on_press: app.add_medication()
+
+    MDRaisedButton:
+        text: "Iniciar Alarme"
+        on_press: app.start_alarm()
+""")
+
+class MyLayout(BoxLayout):
+    pass
+
+class MedicationApp(MDApp):
+    def build(self):
+        return MyLayout()
+
+    def add_medication(self):
+        medicine = self.root.ids.medicine_input.text
+        time = self.root.ids.time_input.text
+        # Aqui você pode adicionar a lógica para armazenar os medicamentos e horários
+
+    def start_alarm(self):
+        # Aqui você pode adicionar a lógica para iniciar o alarme com base nos medicamentos e horários definidos
+        pass
+
+if __name__ == "__main__":
+    MedicationApp().run()
